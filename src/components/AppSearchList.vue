4c58650f8bc45094e5b9380a4a39e125ed5e3cd4
Francesco-Rianna <maradonino98@hotmail.it>
2024-04-19 12:36:56 +0200

    feat : adding cover to card

--- src/components/AppSearchList.vue
+++ src/components/AppSearchList.vue
@@ -17,6 +17,13 @@ export default {
             let flagUrl =cardInfo.original_language + '.svg';
             return `../src/assets/img/${flagUrl}`;
         },
+
+        getPosterUrl(cardInfo){
+           
+           return "https://image.tmdb.org/t/p/w342" + cardInfo.poster_path;
+        
+           
+        }
     }
 }
 </script>
@@ -25,14 +32,22 @@ export default {
     <div>
         
                 <div class="card" style="width: 18rem;">
-                    <div v-if="cardInfo.title" class="title">{{cardInfo.title }}</div>
-                    <div v-else>{{ cardInfo.name }}</div>
-                    <div v-if="cardInfo.original_title" class="orig-title">{{cardInfo.original_title }}</div>
-                    <div v-else>{{ cardInfo.original_name }}</div>
-                    <img v-if="flagsArray.includes(cardInfo.original_language)" :src="getFlagUrl(cardInfo)" :alt="cardInfo.original_language">
-                    <div v-else>{{ cardInfo.original_language  }}</div>
-                    <div class="vote">{{ Math.ceil(cardInfo.vote_average / 2) }}</div>
+                    <div class="poster">
+                        <img :src="getPosterUrl(cardInfo)" alt>
+                    </div>
+
+
+                    <div class="card-boyd">
+                        <div v-if="cardInfo.title" class="title">{{cardInfo.title }}</div>
+                        <div v-else>{{ cardInfo.name }}</div>
+                        <div v-if="cardInfo.original_title" class="orig-title">{{cardInfo.original_title }}</div>
+                        <div v-else>{{ cardInfo.original_name }}</div>
+                        <img v-if="flagsArray.includes(cardInfo.original_language)" :src="getFlagUrl(cardInfo)" :alt="cardInfo.original_language">
+                        <div v-else>{{ cardInfo.original_language  }}</div>
+                        <div class="vote">{{ Math.ceil(cardInfo.vote_average / 2) }}</div>
+                    </div>
                 </div>
+
          
     </div>
 </template>
@@ -40,7 +55,13 @@ export default {
 <style scoped lang="scss">
 img {
     width: 35px;
-} h1{
-    color: red;
+} 
+
+.poster{
+    img{
+        width: 100%;
+        object-fit: cover;
+    }
 }
+
 </style>
